84c44a7a4fac50c96837bac420fb27cdf91ee32f
chavyleung <chavyleung@gmail.com>
2024-07-12 16:29:39 +0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  redirects: async () => [
+    // process.env.redirect_url is undefined
+    { destination: process.env.redirect_url, permanent: false, source: '/' },
+  ],
+};
+
+module.exports = nextConfig;
