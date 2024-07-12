47db86d4dc73a9c2e6a74685ad7a6532afc67b1f
chavyleung <chavyleung@gmail.com>
2024-07-12 16:55:11 +0800

    process.env

--- next.config.js
+++ next.config.js
@@ -3,9 +3,14 @@ console.log(process.env)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   redirects: async () => [
-    // process.env.redirect_url is undefined
-    { destination: process.env.redirect_url, permanent: false, source: '/' },
+    ...getRedirects(),
   ],
 };
+
+function getRedirects() {
+  return [
+    { destination: process.env.redirect_url, permanent: false, source: '/' },
+  ]
+}
 
 module.exports = nextConfig;
